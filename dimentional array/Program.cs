8f9a4747c3c5107148ced22f347bb8c4274003d8
ANJANA <ANJANA@LAPTOP-VVPASTPV>
2024-10-01 11:56:00 +0530

    Add project files.

---
+++ dimentional array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dimentional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,s;
+            Console.WriteLine("enter the limit");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = new int[n];
+            Console.WriteLine("enter the array elements");
+            for(int i=0;i<n;i++)//index
+            {
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+                s = s + ar[i];
+            }
+            Console.WriteLine("the array elements");
+            for(int i=0;i<n;i++)//index
+            {
+                Console.WriteLine(ar[i]);
+            }
+            Console.WriteLine("sum is:{0}");
+            Console.ReadKey();
+        }
+    }
+}
